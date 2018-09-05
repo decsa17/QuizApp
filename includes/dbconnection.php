19c1c52649e8fb4527fbb2aa8454dca85f27e6f1
decsa17 <decsaclaire17@gmail.com>
2018-09-06 02:01:31 +0800

    Create dbconnection.php

---
+++ includes/dbconnection.php
@@ -0,0 +1,10 @@
+
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "decsa017";
+$dbName = "student";
+
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
